b02e9da281d5d4b3ee5ceec71b214251e2570be0
[Muahmmad Saadullah] <msaadullah2022@gmail.com>
2025-03-09 12:58:49 +0500

    Removed unnecessary files

--- debug.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def install_package(package):
-    subprocess.run(["pip", "install", package])
-
-install_package("python-dotenv")
-
-import dotenv
-print("dotenv is installed and imported successfully!")
